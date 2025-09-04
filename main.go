e497058881ee39918c5e828cc2e628fc49f0fbbe
Kirill <marchenko.kiri11@yandex.ru>
2025-09-04 22:20:59 +0300

    refactor: update logic for imt cli

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("Hello go")
+	const power = 2
+
+	var userHeight float64
+	var userWeight float64
+
+	fmt.Println("**Калькулятор индекса веса**")
+	fmt.Print("Введите свой рост в см:")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в кг:")
+	fmt.Scan(&userWeight)
+
+	IMT := userWeight / math.Pow(userHeight/100, power)
+
+	fmt.Printf("Ваш индекс: %.0f\n", IMT)
+
 }
